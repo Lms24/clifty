70349055e04f3c396afa935e113def75b003b20e
Lukas Stracke <lukas@stracke.co.at>
2025-04-22 22:28:45 +0200

    test cmd

--- test/npm.test.ts
+++ test/npm.test.ts
@@ -22,6 +22,8 @@ describe("NPM init", async () => {
     .respondWith(KEYS.ENTER)
     .whenAsked("entry point:")
     .respondWith("index.js", KEYS.ENTER)
+    .whenAsked("test command:")
+    .respondWith(KEYS.ENTER)
     .whenAsked("git repository:")
     .respondWith(KEYS.ENTER)
     .whenAsked("keywords:")
@@ -71,6 +73,7 @@ describe("NPM init with steps", async () => {
       whenAsked("version:").respondWith("1.1.1", KEYS.ENTER);
       whenAsked("description:").respondWith(KEYS.ENTER);
       whenAsked("entry point:").respondWith("index.js", KEYS.ENTER);
+      whenAsked("test command:").respondWith(KEYS.ENTER);
     })
     .step("NPM registry metadata", (whenAsked) => {
       whenAsked("git repository:").respondWith(KEYS.ENTER);
